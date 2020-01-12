4f61a0384cf11c62572c7c42ce17f0855668f194
dkdk443 <demo_weeeds_470@yahoo.co.jp>
2020-01-12 13:54:40 +0900

    Policyを用いた権限実装

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -9,6 +9,10 @@ use App\Http\Requests\AskQuestionRequest;
 
 class QuestionsController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth', ['except' => ['index', 'show']]);
+    }
     /**
      * Display a listing of the resource.
      *
@@ -73,6 +77,7 @@ class QuestionsController extends Controller
      */
     public function edit(Question $question)
     {
+        $this->authorize('update', $question);
         return view('questions.edit', compact('question'));
     }
 
@@ -85,6 +90,7 @@ class QuestionsController extends Controller
      */
     public function update(AskQuestionRequest $request, Question $question)
     {
+        $this->authorize('update', $question);
         $question->update($request->only('title', 'body'));
         return redirect('/questions')->with('success', "質問が変更
         されました");
@@ -98,6 +104,7 @@ class QuestionsController extends Controller
      */
     public function destroy(Question $question)
     {
+        $this->authorize('delete', $question);
         $question->delete();
 
         return redirect('/questions')->with('success', "質問が削除
--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -38,12 +38,20 @@
                     {{-- {{route('question.show', $question->id)}} --}}
                   <a href="{{ $question->url }}"> {{ $question->title }}</a></h3>
                   <div class="ml-auto">
-                    <a href="{{ route('questions.edit', $question->id)}}" class="btn btn-sm btn-outline-info">編集</a>
+                    {{-- @if(Auth::user()->can('update', $question)) --}}
+                    @can('update', $question)
+                      <a href="{{ route('questions.edit', $question->id)}}" class="btn btn-sm btn-outline-info">編集</a>
+                    @endcan
+                    {{-- @endif --}}
+                    
+                    {{-- @if(Auth::user()->can('delete', $question)) --}}
+                    @can('delete', $question)
                   　  <form action="{{ route('questions.destroy', $question->id)}}" method="post">
                       @method('DELETE')
                       @csrf
                       <button type="submit" class="btn btn-sm btn-outline-danger" onclick="return confirm('本当に削除しますか？')">削除</button>
                    </form>
+                   @endcan
                   </div>
                 </div>
                 {{-- ↑urlの定義：モデル --}}
